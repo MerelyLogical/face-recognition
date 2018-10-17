423ad643fb702f30e02caf0d5f708774a3826ace
Edward Wu <36960664+zw4315@users.noreply.github.com>
2018-10-17 09:47:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ mat = sio.loadmat('face.mat')
 data = mat.get("X", "Data read error")
 label = mat.get("l", "Label read error")
 
+data_label = np.vstack([data, label])
+data_train, data_test = train_test_split( data_label.transpose(), test_size=0.2, random_state=42)  #random_state split the data in a certain way
+data_train = data_train.transpose()
+data_test = data_test.transpose()
+
 for i in range(0, 50):
     pic = np.swapaxes(np.reshape(np.array(data[:,i]), (46, 56)), 0, 1)
     plt.subplot(5,10,i+1)
