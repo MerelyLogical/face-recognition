f4e66f3820d794a1ce11997121bfb4a09dc33372
Edward Wu <36960664+zw4315@users.noreply.github.com>
2018-10-17 15:52:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,22 @@ data_train, data_test = train_test_split( data_label.transpose(), test_size=0.2,
 data_train = data_train.transpose()
 data_test = data_test.transpose()
 
+label_train = data_train[2576,:]
+label_test = data_test[2576,:]
+data_train = np.delete(data_train, 2576, 0)
+data_test = np.delete(data_test, 2576, 0)
+
+
+mean_face = np.mean(data_train, axis=1) 
+
+phi_matrix = data_train[:,0] - mean_face
+for i in range (1,data_train.shape[1]):
+   column_sub = data_train[:,i] - mean_face
+   phi_matrix = np.column_stack((phi_matrix, column_sub))
+   
+
+S = (1/phi_matrix.shape[1])* np.dot(phi_matrix, phi_matrix.transpose())
+
 for i in range(0, 50):
     pic = np.swapaxes(np.reshape(np.array(data[:,i]), (46, 56)), 0, 1)
     plt.subplot(5,10,i+1)
