bdca2f16689efa528a23fa98baee33b0de8c15e7
Edward Wu <36960664+zw4315@users.noreply.github.com>
2018-11-09 16:12:21 +0000

    Update lda.py

--- lda.py
+++ lda.py
@@ -6,10 +6,8 @@ Created on Thursday 8th Nov 20:40:54 2018
 """
 # Q3 LDA Ensemble for Face Recognition 
 # PCA-LDA
-import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 
 #load the variables from files
 data_train = np.load("data_train.npy");
@@ -24,8 +22,12 @@ eivec = np.load("eivec.npy");
 distinct_class = np.unique(label_train)                           # types of distinct faces
 count_diffclass =np.zeros(len(distinct_class))                      # count amount of faces for each class
 sum_mi = np.zeros(shape=(2576,1))                      # sum of faces for the same class
-m = np.empty(shape=(2576,1))                              # array of class mean in D dimensional space
+Si = np.zeros(shape=(2576,2576)) 
+Sw = np.zeros(shape=(2576,2576)) 
+Sb = np.zeros(shape=(2576,2576)) 
+m_arr = np.empty(shape=(2576,1))                              # array of class mean in D dimensional space
 count = 0
+m = np.mean(data_train, axis=1) 
 
 for element in range(len(distinct_class)):
     for i in range (len(label_train)):
@@ -34,12 +36,42 @@ for element in range(len(distinct_class)):
            count = count + 1
     count_diffclass[element] = count
     mi = sum_mi / count                                       #single class mean in D dimensional space
-    m = np.column_stack((m, mi))                              #stack mi into an array m
+    Sb = np.outer((mi-m),(mi-m)) + Sb       
+    m_arr = np.column_stack((m_arr, mi))                              #stack mi into an array m
     count = 0
-class_vs_amount =  np.column_stack((distinct_class, count_diffclass))   
+m_arr = np.delete(m_arr,0,1)
+class_vs_amount =  np.column_stack((distinct_class, count_diffclass))  
+
+
+for element in range(len(distinct_class)):
+    for i in range (len(label_train)): 
+        if distinct_class[element] == label_train[i]:
+            Si = np.outer((data_train[:,i] - m_arr[:,element]),(data_train[:,i] - m_arr[:,element])) + Si           
+    Sw = Sw + Si
+    Si = np.zeros(shape=(2576,2576))        
+
+S_lda = np.dot(np.linalg.inv(Sw),Sb) 
+eival_lda, eivec_lda = np.linalg.eig(S_lda)
+
+M_lda = 20
+eivec_lda = eivec_lda.transpose()          # transpose it to sort the eigen vectors
+indexEigenvalue_lda = np.argsort(eival_lda)
+ldaEigenval = eival_lda[indexEigenvalue_lda[-M_lda:]]
+ldaEigenvec = eivec_lda[indexEigenvalue_lda[-M_lda:]]
+bestldaEigenvec = ldaEigenvec[::-1]     # print the eigenface with high energy first
+bestldaEigenval = abs(ldaEigenval[::-1]) 
 
 for i in range(0, 20):
-    pic = np.swapaxes( np.reshape( np.array(m[:,i+1]), (46, 56) ), 0, 1)
+    pic = np.swapaxes( np.reshape( np.array(bestldaEigenvec[i,:]), (46, 56) ), 0, 1)
     plt.subplot(4,5,i+1)
     plt.axis('off')
     plt.imshow(abs(pic), cmap='gray')
+    
+    
+"""    
+for i in range(0, 20):
+    pic = np.swapaxes( np.reshape( np.array(m_arr[:,i+1]), (46, 56) ), 0, 1)
+    plt.subplot(4,5,i+1)
+    plt.axis('off')
+    plt.imshow(abs(pic), cmap='gray')
+"""
