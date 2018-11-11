04c30657ca9a20fcb5b1ec458671efce343b86db
Edward Wu <36960664+zw4315@users.noreply.github.com>
2018-11-11 16:59:53 +0000

    Update new_split.py

--- new_split.py
+++ new_split.py
@@ -9,27 +9,26 @@ Created on Wed Oct 24 20:40:54 2018
 import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 
 mat = sio.loadmat('face.mat')
 data = mat.get("X", "Data read error")
 label = mat.get("l", "Label read error")
 
-data_label = np.vstack([data, label])
-data_train, data_test = train_test_split( data_label.transpose(), test_size=0.2, random_state=42)  #random_state split the data in a certain way
+np.random.seed(42)
 
+data_label = np.vstack([data, label]).reshape(2577, 52, 10)
+for i in range (52):
+    class_slice = data_label[:, i, :].transpose()
+    np.random.shuffle(class_slice)
 
-data_train = data_train.transpose()
-data_test = data_test.transpose()
-
+data_train = np.concatenate(data_label[:, :, 0:8].transpose(), 0).transpose()
+data_test = np.concatenate(data_label[:, :, 8:10].transpose(), 0).transpose()
 
 
 label_train = data_train[2576,:]
 label_test = data_test[2576,:]
 data_train = np.delete(data_train, 2576, 0)
 data_test = np.delete(data_test, 2576, 0)
-
-
 
 #Computing the eigenvalue and eigenvector of the training data, storing them in files
 mean_face = np.mean(data_train, axis=1)        
@@ -60,4 +59,4 @@ np.save("LDeivec.npy", LDeivec);
 # plots the mean face
 mean_pic = np.swapaxes( np.reshape( np.array(mean_face), (46, 56) ), 0, 1)
 plt.axis('off')
-plt.imshow(mean_pic, cmap='gray') 
+plt.imshow(mean_pic, cmap='gray')
