c1db17e60fb0fe77fecf7d5848f5c0db45b7dd84
Edward Wu <36960664+zw4315@users.noreply.github.com>
2018-11-12 17:10:49 +0000

    Update pca_lda.py

--- pca_lda.py
+++ pca_lda.py
@@ -20,7 +20,7 @@ eivec = np.load("eivec2.npy");
 
 #--------------------------------------------------------------------------------
 # Doing PCA first 
-M_pca = 30
+M_pca = 364
 eivec = eivec.transpose()          # transpose it to sort the eigen vectors
 indexEigenvalue = np.argsort(eival)
 pcaEigenval = eival[indexEigenvalue[-M_pca:]]
@@ -29,7 +29,7 @@ bestpcaEigenvec = pcaEigenvec[::-1]     # print the eigenface with high energy f
 bestpcaEigenval = abs(pcaEigenval[::-1]) 
 
 #--------------------------------------------------------------------------------
-#doing LDA 
+
 #find class mean in D-dimensional space
 distinct_class = np.unique(label_train)                           # types of distinct faces
 count_diffclass =np.zeros(len(distinct_class))                      # count amount of faces for each class
@@ -48,19 +48,21 @@ for element in range(len(distinct_class)):
            count = count + 1
     count_diffclass[element] = count
     mi = sum_mi / count                                       #single class mean in D dimensional space
-    Sb = np.outer((mi-m),(mi-m)) + Sb       
+    Sb = np.outer((mi-m),(mi-m)) + Sb
     m_arr = np.column_stack((m_arr, mi))                              #stack mi into an array m
     count = 0
 m_arr = np.delete(m_arr,0,1)
 class_vs_amount =  np.column_stack((distinct_class, count_diffclass))  
 
-
+#Sw_rank = []
 for element in range(len(distinct_class)):
     for i in range (len(label_train)): 
         if distinct_class[element] == label_train[i]:
-            Si = np.outer((data_train[:,i] - m_arr[:,element]),(data_train[:,i] - m_arr[:,element])) + Si           
+            Si = np.outer((data_train[:,i] - m_arr[:,element]),(data_train[:,i] - m_arr[:,element])) + Si
     Sw = Sw + Si
+    #Sw_rank.append(np.linalg.matrix_rank(Sw))
     Si = np.zeros(shape=(2576,2576))        
+
 
 Sw_pca = np.dot(np.dot(bestpcaEigenvec,Sw),bestpcaEigenvec.transpose())
 Sb_pca = np.dot(np.dot(bestpcaEigenvec,Sb),bestpcaEigenvec.transpose())
@@ -68,7 +70,7 @@ Sb_pca = np.dot(np.dot(bestpcaEigenvec,Sb),bestpcaEigenvec.transpose())
 S_lda = np.dot(np.linalg.inv(Sw_pca),Sb_pca) 
 eival_lda, eivec_lda = np.linalg.eig(S_lda)
 
-M_lda = 25
+M_lda = 51
 eivec_lda = eivec_lda.transpose()          # transpose it to sort the eigen vectors
 indexEigenvalue_lda = np.argsort(eival_lda)
 ldaEigenval = eival_lda[indexEigenvalue_lda[-M_lda:]]
@@ -76,9 +78,13 @@ ldaEigenvec = eivec_lda[indexEigenvalue_lda[-M_lda:]]
 bestldaEigenvec = ldaEigenvec[::-1]     # print the eigenface with high energy first
 bestldaEigenval = abs(ldaEigenval[::-1]) 
 
+# force printing by recovering pca
+fish = np.dot(bestldaEigenvec, bestpcaEigenvec)
+
 for i in range(0, 20):
-    pic = np.swapaxes( np.reshape( np.array(bestldaEigenvec[i,:]), (46, 56) ), 0, 1)
+    pic = np.swapaxes( np.reshape( np.array(fish[i,:]), (46, 56) ), 0, 1)
     plt.subplot(4,5,i+1)
     plt.axis('off')
     plt.imshow(abs(pic), cmap='gray')
     
+# test
